993ba2f61cbd06a2e2310aa0ce7a928cb40352de
Sergio <91640683+SergioRReal@users.noreply.github.com>
2021-12-09 20:00:59 -0300

    setter y getter
    agregue seter y geter al usuario

--- src/main/java/com/mycompany/empezaraprobar/Usuario.java
+++ src/main/java/com/mycompany/empezaraprobar/Usuario.java
@@ -12,4 +12,22 @@ package com.mycompany.empezaraprobar;
 public class Usuario {
     private String nombre;
     private String apellido;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
 }
